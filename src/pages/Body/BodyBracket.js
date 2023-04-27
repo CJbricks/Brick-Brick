c6ffb6be09f581c7b9efe227c08f7bbfc9c6dec5
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-04-27 08:37:45 -0600

    Imported files from Lefty

--- src/pages/Body/BodyBracket.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BodyBracket() {
-  return (
-    <div>BodyBracket</div>
-  )
-}
--- src/pages/Body/WebPages.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function WebPages() {
-  return (
-    <div>WebPages</div>
-  )
-}
